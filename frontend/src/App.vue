eb6a5750681f2785a7160b891c7aaa3b25002fdc
Tom Reversez <tom.reversez@gmail.com>
2023-01-25 10:17:58 +0100

    add basic frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import todoList from "./components/todoList.vue"
+</script>
+
+<template>
+  <div>
+    <h1>TO DO LIST APP</h1>
+    <hr/>
+    <todoList/>
+  </div>
+</template>
---
+++ frontend/src/components/todoList.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from "vue"
+const todoList = ref()
+todoList.value = [
+  {
+    id: "0",
+    text: "Une action a réalisée",
+  },
+  {
+    id: "1",
+    text: "Une autre action a réalisée",
+  },
+  {
+    id: "2",
+    text: "Une action a réalisée",
+  },
+  {
+    id: "3",
+    text: "Une autre action a réalisée",
+  },
+]
+</script>
+
+<template>
+  <ul>
+    <li v-for="todo in todoList">
+      {{ todo.text }}
+    </li>
+  </ul>
+</template>
