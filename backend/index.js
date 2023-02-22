1a2840185125651a7307f5f65ad7e8987e6d1eee
Loadeksdi <nicolas.load@gmail.com>
2023-02-22 12:15:04 +0100

    Adding PUT method
    Co-authored-by: Tom Reversez <tomReve@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -11,15 +11,28 @@ app.use(router.routes())
 app.use(router.allowedMethods())
 
 router.get('/api/todos', async (ctx, next) => {
-    ctx.body = await sql`select * from todos`
+    const todos = await sql`select * from todos`
+    console.log(`Todos found: ${todos}`);
+    ctx.body = todos;
 }).post('/api/todos', koaBody(), async (ctx, next) => {
     console.log(ctx.request.body)
     const { title, completed } = ctx.request.body
     ctx.body = await sql`insert into todos (title, completed) values (${title}, ${completed})`
+    ctx.status = 201
+    console.log(`Successfully created todo with payload: ${title} and ${completed}`);
+}).put('/api/todos/:id', koaBody(), async (ctx, next) => {
+    const { id } = ctx.params
+    const { completed } = ctx.request.body
+    await sql`update todos set completed = ${completed} where id = ${id}`
+    console.log(`Successfully updated todo with id: ${id} and completed: ${completed}`);
+    ctx.status = 200
 }).delete('/api/todos/:id', async (ctx, next) => {
     const { id } = ctx.params
     await sql`delete from todos where id = ${id}`
     ctx.status = 204
+    console.log(`Successfully deleted todo with id: ${id}`);
 });
 
+console.log('Server listening on port 3000');
+console.log(process.env.POSTGRES_URL);
 app.listen(3000)
